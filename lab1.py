02263a9e0505460be35adbda644b59996ece00d9
Rayman005 <49414274+Rayman005@users.noreply.github.com>
2019-12-11 16:54:12 +0200

    lab2,v.5

--- lab1.py
+++ lab1.py
@@ -1,16 +1,14 @@
 import random
-
-class Lab1:
     
-    def func (v):
-        sum += i for i in v
-        return {'sum': sum, 'avg': sum/len(v)}
+def func (v):
+    sum += i for i in v
+    return {'sum': sum, 'avg': sum/len(v)}
 
 def main():
     v = [int(random.random()*100)-50 for i in range(int(random.random()*10)+10)]
 
     print(v)
-    print(Lab1.func(v)) 
+    print(func(v)) 
 
 if __name__ == "__main__":
     main()
